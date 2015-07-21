be9c21d2b56170377ab4eb45e8ad9fc8ad84f5e4
juanchel <juanchen.li@redbeacon.com>
2015-07-21 09:41:32 -0700

    read from dict

--- rhymer.go
+++ rhymer.go
@@ -1,9 +1,15 @@
-package main
+package rhymer
 
 import (
     "fmt"
-    "io/ioutil"
+    "os"
+    "bufio"
+    "strings"
 )
+
+type Pronounce struct {
+    phonemes []string
+}
 
 func check(e error) {
     if e != nil {
@@ -12,8 +18,24 @@ func check(e error) {
 }
 
 func main() {
-    fmt.Println("Reading dictionary")
-    dat, err := ioutil.ReadFile("cmudict/cmudict")
+    // Read the file
+    file, err := os.Open("cmudict/cmudict")
     check(err)
-    fmt.Println(string(dat))
+    scanner := bufio.NewScanner(file)
+    defer file.Close()
+    check(err)
+
+    dictionary := make(map[string][]Pronounce)
+
+    // Scan the file line by line
+    for scanner.Scan() {
+        // Split the line by whitespace
+        f := strings.Fields(scanner.Text())
+        // f[0] is the string, and f[2:] is the pronounciation
+
+        dictionary[f[0]] = []Pronounce{Pronounce{f[2:]}}
+    }
+
+    // fmt.Println(dictionary)
+    fmt.Println(dictionary["HELLO"][0].phonemes[2])
 }
