7dc69e6c913cf5e7de63e29b514975c8e196126a
juanchel <juanchen.li@redbeacon.com>
2015-10-08 11:00:09 -0700

    Finish pronounce tests

--- rhymer_test.go
+++ rhymer_test.go
@@ -32,13 +32,6 @@ var rhymeTests = []struct {
     {"^cat",   "&bat",    -1},
 }
 
-var pronounceTests = []struct {
-    a string          // input
-    expected []string // expected result
-} {
-    {"cat", }
-}
-
 func TestMain(m *testing.M) {
     os.Exit(m.Run())
 }
@@ -52,8 +45,45 @@ func TestRhymes(m *testing.T) {
     }
 }
 
-func TestPronounce(m *testing.T) {
-
+func TestPronounceSimple(m *testing.T) {
+    expected := [3]string{"K", "AE", "T"}
+    actual := r.Pronounce("cat")
+    if len(actual) != 1 {
+        m.Errorf("Pronounce(cat) returned the wrong number of results: %d", len(actual))
+    } else if len(actual[0]) != 3 {
+        m.Errorf("Pronounce(cat) returned the wrong number of phonemes: %d")
+    } else {
+        for i, v := range actual[0] {
+            if v != expected[i] {
+                m.Errorf("Pronounce(cat) returned the wrong phoneme at index %d: got %v, expected %v", i, actual[0], expected)
+            }
+        }
+    }
 }
 
-func 
+func TestPronounceMultiple(m *testing.T) {
+    expectedA := [3]string{"AE", "N", "T"}
+    expectedB := [3]string{"AO", "N", "T"}
+    actual := r.Pronounce("aunt")
+    if len(actual) != 2 {
+        m.Errorf("Pronounce(aunt) returned the wrong number of results: %d", len(actual))
+    } else if len(actual[0]) != 3 || len(actual[1]) != 3 {
+        m.Errorf("Pronounce(aunt) returned the wrong number of phonemes")
+    } else {
+        for i := range actual[0] {
+            if actual[0][i] != expectedA[i] && actual[0][i] != expectedB[i] {
+                m.Errorf("Pronounce(aunt) returned the wrong phoneme: got %v, expected [[%v] [%v]]", actual, expectedA, expectedB)
+            }
+            if actual[1][i] != expectedA[i] && actual[1][i] != expectedB[i] {
+                m.Errorf("Pronounce(aunt) returned the wrong phoneme: got %v, expected [[%v] [%v]]", actual, expectedA, expectedB)
+            }
+        }
+    }
+}
+
+func TestPronounceNotFound(m *testing.T) {
+    actual := r.Pronounce("naenae")
+    if len(actual) != 0 {
+        m.Errorf("Pronounce(naenae) should have returned nothing but returned: %v", actual)
+    }
+}
