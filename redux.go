6b329922190a920e3d56e7cd9006295babeb4ef8
juanchel <juanchen.li@redbeacon.com>
2015-10-15 11:27:56 -0700

    Format code with go fmt

--- redux.go
+++ redux.go
@@ -1,76 +1,75 @@
 package rhymer
 
 import (
-    "os"
-    "bufio"
-    "strings"
-    "runtime"
-    "path"
+	"bufio"
+	"os"
+	"path"
+	"runtime"
+	"strings"
 )
 
 // This function converts the cmudict into a slimmer format while throwing away information we don't need
 func redux() {
-    r := new(rhymer)
+	r := new(rhymer)
 
-    // Read the file
-    _, filename, _, _ := runtime.Caller(0)
-    file, err := os.Open(path.Join(path.Dir(filename), "data", "cmudict"))
-    check(err)
-    scanner := bufio.NewScanner(file)
-    defer file.Close()
-    check(err)
+	// Read the file
+	_, filename, _, _ := runtime.Caller(0)
+	file, err := os.Open(path.Join(path.Dir(filename), "data", "cmudict"))
+	check(err)
+	scanner := bufio.NewScanner(file)
+	defer file.Close()
+	check(err)
 
-    r.dictionary = make(map[string][][]string)
+	r.dictionary = make(map[string][][]string)
 
-    // Scan the file line by line
-    for scanner.Scan() {
-        // Split the line by whitespace
-        f := strings.Fields(scanner.Text())
-        // Trim the stress numbers
-        for i, v := range f[2:] {
-            f[i+2] = strings.TrimRight(v, "012")
-        }
-        // f[0] is the string, and f[2:] is the pronounciation
-        r.dictionary[f[0]] = append(r.dictionary[f[0]], f[2:])
-    }
+	// Scan the file line by line
+	for scanner.Scan() {
+		// Split the line by whitespace
+		f := strings.Fields(scanner.Text())
+		// Trim the stress numbers
+		for i, v := range f[2:] {
+			f[i+2] = strings.TrimRight(v, "012")
+		}
+		// f[0] is the string, and f[2:] is the pronunciation
+		r.dictionary[f[0]] = append(r.dictionary[f[0]], f[2:])
+	}
 
-    // Open new file for writing
-    f, err := os.Create(path.Join(path.Dir(filename), "data", "reduxdict"))
-    check(err)
-    defer f.Close()
+	// Open new file for writing
+	f, err := os.Create(path.Join(path.Dir(filename), "data", "reduxdict"))
+	check(err)
+	defer f.Close()
 
-    // For all words currently known
-    for k, v := range r.dictionary {
-        unique := make([][]string, 0)
-        for _, pronounce := range v {
-            skip := false
-            // Check to make sure we don't already have the same pronounciation
-            for _, existing := range unique {
-                if samePhonemes(pronounce, existing) {
-                    skip = true
-                }
-            }
-            if !skip {
-                f.WriteString(k + " " + strings.Join(pronounce, " ") + "\n")
-                unique = append(unique, pronounce)
-            }
-        }
-    }
+	// For all words currently known
+	for k, v := range r.dictionary {
+		unique := make([][]string, 0)
+		for _, pronounce := range v {
+			skip := false
+			// Check to make sure we don't already have the same pronunciation
+			for _, existing := range unique {
+				if samePhonemes(pronounce, existing) {
+					skip = true
+				}
+			}
+			if !skip {
+				f.WriteString(k + " " + strings.Join(pronounce, " ") + "\n")
+				unique = append(unique, pronounce)
+			}
+		}
+	}
 
-    f.Sync()
+	f.Sync()
 }
-
 
 // This essentially checks if two arrays of strings are the same value
 func samePhonemes(n, m []string) bool {
-    if len(n) != len(m) {
-        return false
-    } else {
-        for i, _ := range n {
-            if n[i] != m[i] {
-                return false
-            }
-        }
-        return true
-    }
+	if len(n) != len(m) {
+		return false
+	} else {
+		for i, _ := range n {
+			if n[i] != m[i] {
+				return false
+			}
+		}
+		return true
+	}
 }
--- rhymer.go
+++ rhymer.go
@@ -3,252 +3,252 @@
 package rhymer
 
 import (
-    "os"
-    "bufio"
-    "strings"
-    "runtime"
-    "path"
+	"bufio"
+	"os"
+	"path"
+	"runtime"
+	"strings"
 )
 
 type phonTrie struct {
-    leaves map[string]*phonTrie
-    words map[string]bool
+	leaves map[string]*phonTrie
+	words  map[string]bool
 }
 
 type rhymer struct {
-    dictionary map[string][][]string
-    trie phonTrie
+	dictionary map[string][][]string
+	trie       phonTrie
 }
 
-// Create a new rhymer by reading the pronounciation dictionary
+// Create a new rhymer by reading the pronunciation dictionary
 func NewRhymer() *rhymer {
-    r := new(rhymer)
+	r := new(rhymer)
 
-    // Read the file
-    _, filename, _, _ := runtime.Caller(0)
-    file, err := os.Open(path.Join(path.Dir(filename), "data", "reduxdict"))
-    check(err)
-    scanner := bufio.NewScanner(file)
-    defer file.Close()
-    check(err)
+	// Read the file
+	_, filename, _, _ := runtime.Caller(0)
+	file, err := os.Open(path.Join(path.Dir(filename), "data", "reduxdict"))
+	check(err)
+	scanner := bufio.NewScanner(file)
+	defer file.Close()
+	check(err)
 
-    r.dictionary = make(map[string][][]string)
-    r.trie.leaves = make(map[string]*phonTrie)
+	r.dictionary = make(map[string][][]string)
+	r.trie.leaves = make(map[string]*phonTrie)
 
-    // Scan the file line by line
-    for scanner.Scan() {
-        // Split the line by whitespace
-        f := strings.Fields(scanner.Text())
+	// Scan the file line by line
+	for scanner.Scan() {
+		// Split the line by whitespace
+		f := strings.Fields(scanner.Text())
 
-        // Build the dictionary for quick lookups
-        // f[0] is the string, and f[1:] is the pronounciation
-        r.dictionary[f[0]] = append(r.dictionary[f[0]], f[1:])
+		// Build the dictionary for quick lookups
+		// f[0] is the string, and f[1:] is the pronunciation
+		r.dictionary[f[0]] = append(r.dictionary[f[0]], f[1:])
 
-        // Reduce the word to the rhyming part for trie insertion
-        rhymeSound := RhymeReduce(f[1:])
+		// Reduce the word to the rhyming part for trie insertion
+		rhymeSound := RhymeReduce(f[1:])
 
-        // Insert the word into the trie
-        cur := &r.trie
-        for i := len(rhymeSound)-1; i >= 0; i-- {
-            if cur.leaves[rhymeSound[i]] == nil {
-                cur.leaves[rhymeSound[i]] = &phonTrie{make(map[string]*phonTrie), make(map[string]bool)}
-            }
-            cur = cur.leaves[rhymeSound[i]]
-            if i == 0 {
-                cur.words[f[0]] = true
-            }
-        }
-    }
-    return r
+		// Insert the word into the trie
+		cur := &r.trie
+		for i := len(rhymeSound) - 1; i >= 0; i-- {
+			if cur.leaves[rhymeSound[i]] == nil {
+				cur.leaves[rhymeSound[i]] = &phonTrie{make(map[string]*phonTrie), make(map[string]bool)}
+			}
+			cur = cur.leaves[rhymeSound[i]]
+			if i == 0 {
+				cur.words[f[0]] = true
+			}
+		}
+	}
+	return r
 }
 
 // Rhymes two phoneme slices
 func RhymesFullPhonetic(a1, a2 []string) bool {
-    // Find the word with less rhymable phonemes
-    var longer []string
-    var shorter []string
+	// Find the word with less rhymable phonemes
+	var longer []string
+	var shorter []string
 
-    if vowelOffset(a1) == -1 || vowelOffset(a2) == -1 {
-        return false
-    }
+	if vowelOffset(a1) == -1 || vowelOffset(a2) == -1 {
+		return false
+	}
 
-    if len(a1)-vowelOffset(a1) > len(a2)-vowelOffset(a2) {
-        longer = a1
-        shorter = a2
-    } else {
-        longer = a2
-        shorter = a1
-    }
+	if len(a1)-vowelOffset(a1) > len(a2)-vowelOffset(a2) {
+		longer = a1
+		shorter = a2
+	} else {
+		longer = a2
+		shorter = a1
+	}
 
-    return rhymeTo(longer, shorter)
+	return rhymeTo(longer, shorter)
 }
 
 // Reduce phonemes to the rhyming part
 func RhymeReduce(phon []string) []string {
-    var res []string
-    vowelFound := false
-    for _, v := range phon {
-        if len(v) == 0 {
-            return res
-        }
-        if !vowelFound {
-            switch v[0] {
-            case 'A', 'E', 'I', 'O', 'U':
-                vowelFound = true
-                res = []string{v}
-            }
-        } else {
-            res = append(res, v)
-        }
-    }
-    return res
+	var res []string
+	vowelFound := false
+	for _, v := range phon {
+		if len(v) == 0 {
+			return res
+		}
+		if !vowelFound {
+			switch v[0] {
+			case 'A', 'E', 'I', 'O', 'U':
+				vowelFound = true
+				res = []string{v}
+			}
+		} else {
+			res = append(res, v)
+		}
+	}
+	return res
 }
 
 // Reduce phonemes to the very last rhymable chain
 func SyllabicReduce(phon []string) []string {
-    if len(phon) == 0 {
-        return []string{}
-    }
-    var res []string
-    vowelFound := false
-    for i := len(phon) - 1; i >= 0; i-- {
-        if len(phon[i]) == 0 {
-            return []string{}
-        }
-        switch phon[i][0] {
-        case 'A', 'E', 'I', 'O', 'U':
-            vowelFound = true
-            res = append([]string{phon[i]}, res...)
-        default:
-            if vowelFound {
-                return res
-            }
-            res = append([]string{phon[i]}, res...)
-        }
-    }
-    if vowelFound {
-        return res
-    } else {
-        return []string{}
-    }
+	if len(phon) == 0 {
+		return []string{}
+	}
+	var res []string
+	vowelFound := false
+	for i := len(phon) - 1; i >= 0; i-- {
+		if len(phon[i]) == 0 {
+			return []string{}
+		}
+		switch phon[i][0] {
+		case 'A', 'E', 'I', 'O', 'U':
+			vowelFound = true
+			res = append([]string{phon[i]}, res...)
+		default:
+			if vowelFound {
+				return res
+			}
+			res = append([]string{phon[i]}, res...)
+		}
+	}
+	if vowelFound {
+		return res
+	} else {
+		return []string{}
+	}
 }
 
-// Find all words that rhyme with slice of pronounciation phonemes
+// Find all words that rhyme with slice of pronunciation phonemes
 func (r *rhymer) FindRhymes(s []string) []string {
-    rhymeSound := RhymeReduce(s)
-    cur := &r.trie
-    for i := len(rhymeSound)-1; i >= 0; i-- {
-        if cur.leaves[rhymeSound[i]] != nil {
-            cur = cur.leaves[rhymeSound[i]]
-        } else {
-            return []string{}
-        }
-    }
-    return cur.getFullSet()
+	rhymeSound := RhymeReduce(s)
+	cur := &r.trie
+	for i := len(rhymeSound) - 1; i >= 0; i-- {
+		if cur.leaves[rhymeSound[i]] != nil {
+			cur = cur.leaves[rhymeSound[i]]
+		} else {
+			return []string{}
+		}
+	}
+	return cur.getFullSet()
 }
 
-// Get the pronounciation phonemes for string s
+// Get the pronunciation phonemes for string s
 func (r *rhymer) Pronounce(s string) [][]string {
-    return r.dictionary[strings.ToUpper(s)]
+	return r.dictionary[strings.ToUpper(s)]
 }
 
 // Find all words that rhyme with string s
 func (r *rhymer) FindRhymesByWord(s string) []string {
-    s = strings.ToUpper(s)
-    if _, ok := r.dictionary[s]; !ok {
-        return []string{}
-    }
-    return r.FindRhymes(r.dictionary[s][0])
+	s = strings.ToUpper(s)
+	if _, ok := r.dictionary[s]; !ok {
+		return []string{}
+	}
+	return r.FindRhymes(r.dictionary[s][0])
 }
 
 // Checks wheter s rhymes with a phoneme slice p1
 func (r *rhymer) RhymesPhonetic(s string, p1 []string) int {
-    s = strings.ToUpper(s)
-    p2 := r.Pronounce(s)
+	s = strings.ToUpper(s)
+	p2 := r.Pronounce(s)
 
-    if len(p2) == 0 {
-        return -1
-    }
+	if len(p2) == 0 {
+		return -1
+	}
 
-    for _, v := range p2 {
-        if RhymesFullPhonetic(v, p1) {
-            return 1
-        }
-    }
-    return 0
+	for _, v := range p2 {
+		if RhymesFullPhonetic(v, p1) {
+			return 1
+		}
+	}
+	return 0
 }
 
 // Checks whether or not s1 and s2 rhyme, returns 1 if they do, 0 if they don't, and -1 when one word is unknown
 func (r *rhymer) Rhymes(s1, s2 string) int {
-    s1 = strings.ToUpper(s1)
-    s2 = strings.ToUpper(s2)
+	s1 = strings.ToUpper(s1)
+	s2 = strings.ToUpper(s2)
 
-    p1 := r.Pronounce(s1)
-    p2 := r.Pronounce(s2)
+	p1 := r.Pronounce(s1)
+	p2 := r.Pronounce(s2)
 
-    // Return -1 if one of the words is unknown
-    if len(p1) == 0 || len(p2) == 0 {
-        return -1
-    }
+	// Return -1 if one of the words is unknown
+	if len(p1) == 0 || len(p2) == 0 {
+		return -1
+	}
 
-    // Return 1 if any of the prounounciations rhyme
-    for _, v := range p1 {
-        for _, w := range p2 {
-            if RhymesFullPhonetic(v, w) {
-                return 1
-            }
-        }
-    }
-    return 0
+	// Return 1 if any of the prounounciations rhyme
+	for _, v := range p1 {
+		for _, w := range p2 {
+			if RhymesFullPhonetic(v, w) {
+				return 1
+			}
+		}
+	}
+	return 0
 }
 
 // This returns a slice of all the words at the current level of a slice and recurses on its children
 func (p *phonTrie) getFullSet() []string {
-    res := []string{}
-    for k, _ := range p.words {
-        res = append(res, k)
-    }
-    for _, v := range p.leaves {
-        res = append(res, v.getFullSet()...)
-    }
-    return res
+	res := []string{}
+	for k, _ := range p.words {
+		res = append(res, k)
+	}
+	for _, v := range p.leaves {
+		res = append(res, v.getFullSet()...)
+	}
+	return res
 }
 
 // Find how many phonemes the vowel is offset so we know where to start rhyming
 func vowelOffset(s []string) int {
-    for i, v := range s {
-        if len(v) == 0 {
-            return -1
-        }
-        switch v[0] {
-        case 'A', 'E', 'I', 'O', 'U':
-            return i
-        }
-    }
-    return -1
+	for i, v := range s {
+		if len(v) == 0 {
+			return -1
+		}
+		switch v[0] {
+		case 'A', 'E', 'I', 'O', 'U':
+			return i
+		}
+	}
+	return -1
 }
 
 func rhymeTo(l, s []string) bool {
-    diff := len(l) - len(s)
-    ret := true
+	diff := len(l) - len(s)
+	ret := true
 
-    offset := vowelOffset(s)
-    if offset == -1 {
-        return false
-    }
+	offset := vowelOffset(s)
+	if offset == -1 {
+		return false
+	}
 
-    // Check if the words sound the same, ignoring the first constanant sounds of the shorter word
-    for i, v := range s[offset:] {
-        if l[diff+i+offset] != v {
-            ret = false
-        }
-    }
+	// Check if the words sound the same, ignoring the first constanant sounds of the shorter word
+	for i, v := range s[offset:] {
+		if l[diff+i+offset] != v {
+			ret = false
+		}
+	}
 
-    return ret
+	return ret
 }
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
